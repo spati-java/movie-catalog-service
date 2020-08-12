627f3f96a0d32d0f467e473578fe4b67777997ad
Sourav Pati <sourav.pati@objectstream.com>
2020-08-12 13:04:35 -0500

    first commit

---
+++ src/main/java/com/p/moviecatalogservice/model/CatalogItem.java
@@ -0,0 +1,15 @@
+package com.p.moviecatalogservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CatalogItem {
+
+    private String name;
+    private String description;
+    private int rating;
+}
---
+++ src/main/java/com/p/moviecatalogservice/model/Movie.java
@@ -0,0 +1,9 @@
+package com.p.moviecatalogservice.model;
+
+import lombok.Data;
+
+@Data
+public class Movie {
+    private String movieId;
+    private String name;
+}
